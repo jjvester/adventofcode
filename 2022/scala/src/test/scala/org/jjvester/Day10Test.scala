863841cdc600e5d5968dd32c4c9ba92176aac891
Senator Palpatine <jjvester@gmail.com>
2022-12-10 21:24:39 +0100

    Refactored a bit, so it does not look like the cat threw up in my IDE

--- 2022/scala/src/test/scala/org/jjvester/Day10Test.scala
+++ 2022/scala/src/test/scala/org/jjvester/Day10Test.scala
@@ -14,16 +14,31 @@ class Day10Test extends munit.FunSuite:
 
   test("part1") {
     val input = InputReader.readInput("src/main/resources/day10/input")
-    //Assert.assertEquals(13140, Day10.part1(input))
-    println(Day10.part1(input))
+    Assert.assertEquals(12540, Day10.part1(input))
   }
 
   test("Sample_part2") {
     val input = InputReader.readInput("src/main/resources/day10/sample")
-    Day10Part2.part1(input)
+    Day10.part2(input)
+    /*
+    # # . . # # . . # # . . # # . . # # . . # # . . # # . . # # . . # # . . # # . .
+    # # # . . . # # # . . . # # # . . . # # # . . . # # # . . . # # # . . . # # # .
+    # # # # . . . . # # # # . . . . # # # # . . . . # # # # . . . . # # # # . . . .
+    # # # # # . . . . . # # # # # . . . . . # # # # # . . . . . # # # # # . . . . .
+    # # # # # # . . . . . . # # # # # # . . . . . . # # # # # # . . . . . . # # # #
+    # # # # # # # . . . . . . . # # # # # # # . . . . . . . # # # # # # # . . . . .
+    */
   }
 
   test("part2") {
     val input = InputReader.readInput("src/main/resources/day10/input")
-    Day10Part2.part1(input)
+    Day10.part2(input)
+    /*
+    # # # # . # # # # . . # # . . # # # # . # # # # . # . . . . # . . # . # # # # .
+    # . . . . # . . . . # . . # . . . . # . # . . . . # . . . . # . . # . # . . . .
+    # # # . . # # # . . # . . . . . . # . . # # # . . # . . . . # # # # . # # # . .
+    # . . . . # . . . . # . . . . . # . . . # . . . . # . . . . # . . # . # . . . .
+    # . . . . # . . . . # . . # . # . . . . # . . . . # . . . . # . . # . # . . . .
+    # . . . . # # # # . . # # . . # # # # . # # # # . # # # # . # . . # . # # # # .
+    */
   }
